5c0178289e8102f8bffebffca93e91e820b11a25
leejh2086 <132995715+leejh2086@users.noreply.github.com>
2023-05-26 16:57:42 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 3500,
+      disableOnInteraction: false,
+    },
+    
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
